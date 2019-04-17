26bae3adabfd36622959b4b73409c8d2e8b7c7ce
Maximous000 <49714374+Maximous000@users.noreply.github.com>
2019-04-17 11:20:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`24 Hours Online`,"http://twitch.tv/24hoursonline")
 client.user.setStatus("dnd")
 });
 
